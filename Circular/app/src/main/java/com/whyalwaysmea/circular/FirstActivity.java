c4b058dab5e383e3250d7824f30184ee7c7d61ca
HanLong <404402223@qq.com>
2016-09-22 18:30:17 +0800

    add circular

---
+++ Circular/app/src/main/java/com/whyalwaysmea/circular/FirstActivity.java
@@ -0,0 +1,27 @@
+package com.whyalwaysmea.circular;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        View v = findViewById(R.id.touch_me_view);
+        v.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Intent i = new Intent(FirstActivity.this, SecondActivity.class);
+                i.putExtra("x", (int)event.getX());
+                i.putExtra("y", (int)event.getY());
+                startActivity(i);
+                return false;
+            }
+        });
+    }
+}
---
+++ Circular/app/src/main/java/com/whyalwaysmea/circular/SecondActivity.java
@@ -0,0 +1,100 @@
+package com.whyalwaysmea.circular;
+
+import android.animation.Animator;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private View content;
+    private int mX;
+    private int mY;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        content =  findViewById(R.id.reveal_content);
+        content.post(new Runnable() {
+
+            @Override
+            public void run() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    mX = getIntent().getIntExtra("x", 0);
+                    mY = getIntent().getIntExtra("y", 0);
+                    Animator animator = createRevealAnimator(false, mX, mY);
+                    animator.start();
+                }
+                content.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        content.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    Animator animator = createRevealAnimator(true, (int) event.getX(), (int) event.getY());
+                    animator.start();
+                } else {
+                    finish();
+                }
+                return false;
+            }
+        });
+    }
+
+
+
+    private Animator createRevealAnimator(boolean reversed, int x, int y) {
+        float hypot = (float) Math.hypot(content.getHeight(), content.getWidth());
+        float startRadius = reversed ? hypot : 0;
+        float endRadius = reversed ? 0 : hypot;
+
+        Animator animator = ViewAnimationUtils.createCircularReveal(
+                content, x, y,
+                startRadius,
+                endRadius);
+        animator.setDuration(800);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        if (reversed)
+            animator.addListener(animatorListener);
+        return animator;
+    }
+
+    private Animator.AnimatorListener animatorListener = new Animator.AnimatorListener() {
+        @Override
+        public void onAnimationStart(Animator animation) {
+        }
+
+        @Override
+        public void onAnimationEnd(Animator animation) {
+            content.setVisibility(View.INVISIBLE);
+            finish();
+        }
+
+        @Override
+        public void onAnimationCancel(Animator animation) {
+        }
+
+        @Override
+        public void onAnimationRepeat(Animator animation) {
+        }
+    };
+
+    @Override
+    public void onBackPressed() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Animator animator = createRevealAnimator(true, mX, mY);
+            animator.start();
+        } else {
+            finish();
+        }
+    }
+}
